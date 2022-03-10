380a3f6a37eed3f2c4b64f6534f5cd4d03a0e3b7
glitch-prog <dubovkasfamily@gmail.com>
2022-03-10 14:03:32 +0300

    init commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import logo from './assets/logo.png';
 
 export default function App() {
+  let openImagePickerAsync = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required!');
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.instructions}>
+        To share a photo, press the button below!
+      </Text>
+      <StatusBar style='auto' />
+      <TouchableOpacity
+        onPress={openImagePickerAsync}
+        style={styles.buttonBackground}
+      >
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +41,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: { width: 305, height: 159 },
+  instructions: { color: 'red', fontSize: 18 },
+  buttonBackground: { backgroundColor: 'blue' },
+  buttonText: { fontSize: 20, color: '#fff' },
 });
